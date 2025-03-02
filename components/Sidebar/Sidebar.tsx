14a23157510e84db24e7592b3cdaa407f9d66df5
juan rodriguez <juan.rodriguezs@usantoto.edu.co>
2025-03-01 19:38:01 -0500

    primeiro commiti

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client"; 
+import { Logo } from "@/components/Logo";
+
+import { SidebarRoutes } from "../SidebarRoutes";
+
+export function Sidebar() {
+    return (
+        <div className="h-screen">
+            <div className="flex flex-col h-full border-r">
+                <Logo />
+                <SidebarRoutes />
+            </div>
+        </div>
+    )
+}
