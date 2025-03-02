83a2d89feaca8be7f9c4d9e2386ec089de572bc7
juan rodriguez <juan.rodriguezs@usantoto.edu.co>
2025-03-01 20:47:30 -0500

    changezzz

--- app/(routes)/companies/[companyId]/page.tsx
+++ app/(routes)/companies/[companyId]/page.tsx
@@ -1,16 +1,15 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-
 import { Header } from "./components/Header";
 import { CompanyInformation } from "./components/CompanyInformation";
 import { FooterCompany } from "./components/FooterCompany";
 
+type Params = { companyId: string }; // Define el tipo explícitamente
+
 export default async function CompanyIdPage({
     params,
-}: {
-    params: Record<string, string>;
-}) {
+}: { params: Params }) {
     const { userId } = await auth();
 
     if (!userId) {
